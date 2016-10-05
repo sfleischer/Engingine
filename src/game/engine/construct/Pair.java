095593d2ddd2c3ac52b7b1d382e59517a9c47fa8
sfleischer <fls@seas.upenn.edu>
2016-10-05 02:05:52 -0400

    Added a new generic Pair class

--- src/game/engine/construct/Pair.java
+++ src/game/engine/construct/Pair.java
@@ -1,5 +1,28 @@
 package game.engine.construct;
 
-public class Pair {
-
+/**
+ * This is a generic Pair class
+ * @author sfleischer
+ * @version 10/5/16
+ *
+ * @param <A> The first object in the pair
+ * @param <B> The second object in the pair
+ */
+public class Pair<A, B> {
+	
+	private A body1;
+	private B body2;
+	
+	public Pair(A b1, B b2){
+		body1 = b1;
+		body2 = b2;
+	}
+	
+	public A getFirst(){
+		return body1;
+	}
+	
+	public B getSecond(){
+		return body2;
+	}
 }
