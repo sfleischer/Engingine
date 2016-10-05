f5515b7b1ca251d6e77b0f8819ae2ce813f61462
sfleischer <fls@seas.upenn.edu>
2016-10-05 02:06:11 -0400

    Implemented BruteForce

--- src/game/engine/detection/broad/BruteForce.java
+++ src/game/engine/detection/broad/BruteForce.java
@@ -1,5 +1,20 @@
 package game.engine.detection.broad;
 
-public class BruteForce {
+import java.util.ArrayList;
 
+import game.engine.construct.Body;
+import game.engine.construct.Pair;
+
+public class BruteForce implements Detectable{
+	
+	public ArrayList<Pair<Body, Body>> detect (ArrayList<Body> bodies){
+		
+		ArrayList<Pair<Body, Body>> pairs = new ArrayList<>();
+		for(int i = 0; i < bodies.size(); i++){
+			for(int j = i+1; j < bodies.size(); j++){
+				pairs.add(new Pair<Body, Body>(bodies.get(i), bodies.get(j)));
+			}
+		}
+		return pairs;
+	}
 }
